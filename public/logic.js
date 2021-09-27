a91b57ff9cb744451f4ed85c918d1d04c3a17811
Rusty-nick <rusty-nick@live.com>
2021-09-27 20:24:40 +0200

    append products, home & cart. Styling to products
    Arbetat tillsammans

--- public/logic.js
+++ public/logic.js
@@ -1,24 +1,11 @@
 const publicKey = "pk_test_51Jc4KaFYrsT4JzuLnxfWccQV75y84ogkUy5ptJoxmDaja5LIaydegbHC3I8kszcwiuHrWMa4XOsI6DwEFzsBlcGF00RMkCDsNF"
 //const sessionId = localStorage.getItem(session)
 
-let cart = []
-
-let testCart = [
-    {'boxedIce':{
-    description:'boxed in liquid form',
-    price_data:{
-        currency:'sek',
-        product_data:{
-            name: 'boxedIce'
-        },
-        unit_amount:5000
-    }
-}}
-]
+let cart = {}
 
 let stripe = Stripe(publicKey)
 //document.getElementById("testBtn").addEventListener("click", async () => {
-    
+
 /*     if (Object.keys(testCar).length == 0) {
         console.log("No products")
     }
@@ -41,23 +28,91 @@ let stripe = Stripe(publicKey)
     
 } */
 
-const checkout = async () =>{
+const productsDB = {
+    "liquidIce": {
+        description: "En kul produkt som du bara måste ha",
+        price_data: {
+            currency: "sek",
+            product_data: {
+                name: "liquidIce",
+                metadata: {
+                    img: "./resources/glassOfWater.jpg",
+                }
+            },
+            unit_amount: 500,
+        }
+    },
+    "IceMarble": {
+        description: "Marbles of Ice",
+        price_data: {
+            currency: "sek",
+            product_data: {
+                name: "IceMarble",
+                metadata: {
+                    img: "./resources/iceMarble.jpg",
+                },
+            },
+            unit_amount: 45000,
+        }
+    }
+}
+
+/* let quantity = 0
+function addProduct() {
+    quantity++
+    cart = {
+        'Liquid Ice': {
+            name: "Liquid Ice",
+            description: 'boxed in liquid form',
+            price_data: {
+                currency: 'sek',
+                product_data: {
+                    name: 'boxedIce'
+                },
+                unit_amount: quantity
+            }
+        }
+    }
+    let getCart = localStorage.getItem('cart')
+
+    let setCart = localStorage.setItem("cart", JSON.stringify(cart))
+
+} */
+
+
+const addProduct = async (productKey) => {
+    const product = productsDB[productKey];
+
+    if (!product) {
+        throw new Error('Product does not Exist')
+    }
+
+    cart[productKey] = cart[productKey] || product;
+    cart[productKey].quantity = cart[productKey].quantity || 0;
+    cart[productKey].quantity++;
+    let getCart = localStorage.getItem('cart')
+    let setCart = localStorage.setItem("cart", JSON.stringify(cart))
+    console.log({ cart, line_items: Object.values(cart), setCart })
+
+
+}
+
+const checkout = async () => {
     try {
-        
-        if (Object.keys(testCart).length == 0) {
+        if (Object.keys(cart).length == 0) {
             console.log("No products")
         }
-    
+        console.log(cart)
         const response = await fetch("/api/session/new", {
             method: "POST",
-            headers: { "Content-Type": "application/json" }, 
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                line_items: Object.values(testCart)
+                line_items: Object.values(cart)
             })
         })
         const { id } = await response.json()
         localStorage.setItem("session", id)
-        stripe.redirectToCheckout({ sessionId: id }); 
+        stripe.redirectToCheckout({ sessionId: id });
         return true
     } catch (err) {
         console.error(err)
@@ -68,76 +123,61 @@ const checkout = async () =>{
 let list = "detta är en cool lista av våra is produkter"
 
 async function verify() {
-    try{
-        
+    try {
+
         const sessionId = localStorage.getItem("session")
-        
-        if(!sessionId){
+
+        if (!sessionId) {
             console.log('No session ID to verify')
             //session ID finns inte i Localstorage
         } //Delete
         //console.log(sessionId)
 
-        const response = await fetch('/api/session/verify/' + sessionId,  {
+        const response = await fetch('/api/session/verify/' + sessionId, {
             method: "POST",
-            headers:{'Contenet-Type': 'application/json'},
-            body: JSON.stringify({list: list})       
+            headers: { 'Contenet-Type': 'application/json' },
+            body: JSON.stringify({ list: list })
         })
         /*     body:
             JSON.stringify({
                 sessionId: sessionId 
             }) */
-    
+
         const result = await response.json()
-        
+
         console.log(result)
         const { paid } = await response.json()
 
         return paid;
 
-    }catch (err) {
+    } catch (err) {
         console.error(err)
         return false
     }
 }
 
-let quantity = 0
-function addProduct(){   
-    quantity++
-    cart = {'Liquid Ice':{
-        name:"Liquid Ice",
-        description:'boxed in liquid form',
-        price_data:{
-            currency:'sek',
-            product_data:{
-                name: 'boxedIce'
-            },
-            unit_amount:quantity
-        }
-    }} 
-    let getCart = localStorage.getItem('cart')
-
-    let setCart = localStorage.setItem("cart",JSON.stringify(cart))
-    
-}
 
 
-async function main(){
+async function main() {
+
     //document.getElementById('addbtn').addEventListener('click', addProduct)
-    document.getElementById('checkOutBtn').addEventListener('click', ()=> checkout())
+    //document.getElementById('addbtn').addEventListener('click', () => addProduct('liquidIce'))
+
+
+    /*     if (document.getElementById('checkOutBtn')) {
+            document.getElementById('checkOutBtn').addEventListener('click', () => checkout())
+        } */
     //console.log("isVerified", isVerified)
     localStorage.removeItem("session")
-} 
-
+}
 main()
-
 /* const addProduct = async (productKey) => {
     const product = productsDB[productKey];
     if (!product) {
         throw new Error('Product does not Exist')
     }
-    
-    
+
+
     cart[productKey] = cart[productKey] || product;
     cart[productKey].quantity = cart[productKey].quantity || 0;
     cart[productKey].quantity++;
--- public/successLogic.js
+++ public/successLogic.js
@@ -1,10 +1,24 @@
+function getProducts() {
+    const getCartList = localStorage.getItem('cart')
 
+    let cartlist = JSON.parse(getCartList)
+    console.log(cartlist)
+    return cartlist
+}
 async function success() {
+    let cart = getProducts()
+    let cartItems = Object.values(cart)
+
+    let amountKey = cartItems[0].quantity
+    console.log(amountKey)
     const response = await fetch('/api/recet', {
         method: "POST",
-        headers:{'Contenet-Type': 'application/json'},
-    })
-    
-}
+        headers: { 'Contenet-Type': 'application/json' },
+        body: JSON.stringify({
+            amountKey
+        })
 
+    })
+    localStorage.removeItem("cart")
+}
 success()
